09cccda09d4fa88959a424ad28737ca56d18fd81
Ryan Raub <ryan.raub@asu.edu>
2016-03-26 16:02:05 -0700

    lint errors

--- client/index.js
+++ client/index.js
@@ -6,7 +6,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import todoApp from './reducers';
 import App from './components/App';
 import { store } from './stores';
 
--- client/stores/index.js
+++ client/stores/index.js
@@ -1,7 +1,7 @@
-import { createStore } from 'redux'
-import todoApp from '../reducers'
+import {createStore} from 'redux';
+import todoApp from '../reducers';
 // let store = createStore(todoApp);
-let store = createStore(todoApp, window.STATE_FROM_SERVER)
+let store = createStore(todoApp, window.STATE_FROM_SERVER);
 const dispatch = store.dispatch;
 
 // export default store;
